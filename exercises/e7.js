26a3d88de3560ce06af851412a65649f20d550a8
Cody <codybrunson182@gmail.com>
2025-09-08 06:33:23 -0500

    exercise 7 is complete. Next exercise

--- exercises/e7.js
+++ exercises/e7.js
@@ -5,8 +5,15 @@
 // getClientWithLeastBalance(bankAccounts) => [{ name: 'SomeName', balance: 32, ... }]
 
 export function getClientWithLeastPositiveBalance(array) {
-  // Your code goes here...
-  
+  let minAccount = null;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i].balance > 0) {
+      if (minAccount === null || array[i].balance < minAccount.balance) {
+        minAccount = array[i];
+      }
+    }
+  }
+  return minAccount ? [minAccount] : [];
 }
 
 // === TEST YOURSELF ===
